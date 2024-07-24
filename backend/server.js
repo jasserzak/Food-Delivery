91bc230c76e4488bcf328d613b6c7c4d7d2f849f
Jasserzak <jasserslimani01@gmail.com>
2024-07-24 11:27:08 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ import foodRouter from "./routes/foodRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 // app config
 const app = express();
@@ -24,6 +31,18 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+//Render frontend for any path
+app.get("/admin", (req, res) =>
+  res.sendFile(path.join(__dirname, "../admin/dist/index.html"))
+);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+);
 
 app.get("/", (req, res) => {
   res.send("API Working");
--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import FoodItem from '../FoodItem/FoodItem'
-import { StoreContext } from '../../Context/StoreContext'
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import FoodItem from "../FoodItem/FoodItem";
+import { StoreContext } from "../../Context/StoreContext";
 
-const FoodDisplay = ({category}) => {
-
-  const {food_list} = useContext(StoreContext);
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
 
   return (
-    <div className='food-display' id='food-display'>
+    <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
-      <div className='food-display-list'>
-        {food_list.map((item)=>{
-          if (category==="All" || category===item.category) {
-            return <FoodItem key={item._id} image={item.image} name={item.name} desc={item.description} price={item.price} id={item._id}/>
+      <div className="food-display-list">
+        {food_list.map((item) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={item._id}
+                image={item.image}
+                name={item.name}
+                desc={item.description}
+                price={item.price}
+                id={item._id}
+              />
+            );
           }
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
